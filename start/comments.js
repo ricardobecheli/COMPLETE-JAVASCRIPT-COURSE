aec0e6790f1bd80dfe50b594f8d6e86b7eba28f3
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-03 09:39:24 -0300

    Update comments.js

--- start/comments.js
+++ start/comments.js
@@ -4,13 +4,17 @@
 
 
 console.log(1+3*10);
-console.log("Text always between double quotes or...");
-//console.log('Or simple quotes');
 
-//the above live of code was neutralized by "commenting".
+console.log("Text always between double quotes or..."); //console.log('Or simple quotes');
+
+
+//the above live of code was neutralized (not executed) by the comment.
 
 /* for more than one line comment
 just use this,
 like you've learned in CSS:
 slash asterisk - asterisk slash
 */
+
+// "Use and abuse of comments in the lessons, but only essential comments at work".
+
