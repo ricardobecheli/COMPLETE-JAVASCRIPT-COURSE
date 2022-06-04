c0fb20e903c8c6fec5aed3feeee4c67a1967d037
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-04 17:02:50 -0300

    Create variables.js

---
+++ start/variables.js
@@ -0,0 +1,11 @@
+
+let custo = 10
+let imposto = custo*0.1
+let preco = custo + imposto
+
+
+{
+    console.log(custo);
+    console.log(imposto); // this here is a block of code, between braces.
+    console.log(preco);
+}
