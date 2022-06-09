1add1e78f893218fee3bca985d6f04edcbe90ee3
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-09 10:12:55 -0300

    Code block inside another 2 blocks

---
+++ js-exercises/fundamentals/organization.js
@@ -0,0 +1,13 @@
+{
+
+    {
+
+        {
+            console.log("Bloco de código - Code block inside another 2 blocks, ok.")
+            console.log("Hello")
+            console.log("World!")
+        }
+
+    }
+
+}
