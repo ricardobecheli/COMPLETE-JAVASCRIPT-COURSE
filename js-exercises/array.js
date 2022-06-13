32c382519c919aa613d121ad1127752f7c5a082b
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-13 17:37:33 -0300

    Update array.js

--- js-exercises/array.js
+++ js-exercises/array.js
@@ -1,3 +1,17 @@
+// Array: a neat way of storing a list of data items under a single variable name.
+
+/* 
+Arrays are generally described as "list-like OBJECTS"; they are basically single objects that contain multiple values stored in a list.
+
+Array objects can be stored in variables
+
+and dealt with in much the same way as any other type of value,
+
+the difference being that we can access each value inside the list individually, and do super useful and efficient things with the list, like loop through it and do the same thing to every value. Maybe we've got a series of product items and their prices stored in an array, and we want to loop through them all and print them out on an invoice, while totaling all the prices together and printing out the total price at the bottom.
+
+If we didn't have arrays, we'd have to store every item in a separate variable, then call the code that does the printing and adding separately for each item. This would be much longer to write out, less efficient, and more error-prone. If we had 10 items to add to the invoice it would already be annoying, but what about 100 items, or 1000?
+*/
+
 const notasMensais = [7.7, 8.9, 6.3, 9.2]
 console.log(notasMensais[0], notasMensais[3])
 console.log(notasMensais[1], notasMensais[2])
