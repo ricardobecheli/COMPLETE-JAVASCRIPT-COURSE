b59fa479bf16f5c74f8cc5d4cce5523d1a1ecdb3
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-14 14:12:35 -0300

    Array reviwed - Object created.

--- js-exercises/array.js
+++ js-exercises/array.js
@@ -12,37 +12,48 @@ the difference being that we can access each value inside the list individually,
 If we didn't have arrays, we'd have to store every item in a separate variable, then call the code that does the printing and adding separately for each item. This would be much longer to write out, less efficient, and more error-prone. If we had 10 items to add to the invoice it would already be annoying, but what about 100 items, or 1000?
 */
 
-const notasMensais = [7.7, 8.9, 6.3, 9.2]
-console.log(notasMensais[0], notasMensais[3])
-console.log(notasMensais[1], notasMensais[2])
-console.log(notasMensais[1], notasMensais[4])
-console.log(notasMensais[4])
-console.log(notasMensais)
-console.log(notasMensais.length)
-notasMensais[4]=10.1    // brackets are used for inserting or changing a value.
-console.log(notasMensais)
-console.log(notasMensais.length)
-notasMensais[5]=11
-console.log(notasMensais)
-console.log(notasMensais.length)
-notasMensais.push(12, 13, 14, 15, 16)
-/* "push" inserts new values of any type i.e. nr+str+bool
-but it's better not to mix types of data */
-console.log(notasMensais)
-console.log(notasMensais.length)
-console.log(notasMensais)
-console.log(notasMensais)
-// 2 ways of removing values from the array:
+const notasMensais = [7.7, 8.9, 6.3, 9.2] // four positions in this array: from zero to 3.
 
-// 1 -  "pop" removes the last one:
-console.log(notasMensais.pop()) //displays only the removed value
-console.log(notasMensais)  // displays the new array values
+console.log(notasMensais[0], notasMensais[3])//Displays positions zero and 3.
+console.log(notasMensais[1], notasMensais[2])
+console.log(notasMensais[1], notasMensais[4])//NO position 4. Displays: "8.9 and undefined".
+console.log(notasMensais[4]) // "undefined"
+console.log(notasMensais) // Displays all.
+console.log(notasMensais.length) // Displays how many positions (4, in this case so far).
+
+notasMensais[4]=10.1 // Brackets w/ pos. # are used for inserting or changing a value.
+console.log(notasMensais) // Now it has 1 more (10.1). = position 4.
+
+console.log(notasMensais.length) // Length =5
+
+notasMensais[5]=11
+console.log(notasMensais) // Now it has plus 1 (11). = position 5.
+
+console.log(notasMensais.length) // Length = 6
+
+notasMensais.push(12, 13, 14, 15, 16) /* "push" inserts new values of any type
+    i.e. nr+str+bool
+    but it's better not to mix types of data */
+
+
+console.log(notasMensais) // Now it has plus 5 (12 to 16). = positions 7 through 11.
+
+console.log(notasMensais.length) // Length = 11
+console.log(notasMensais)
+console.log(notasMensais)
+
+
+// There are 2 ways of removing values from the array:
+
+// 1 -  "pop" removes the last value:
+        console.log(notasMensais.pop()) //displays only the removed value - the last one.
+        console.log(notasMensais)  // displays all the array values, now w/o the last one.
+
 
 // 2 - "delete" thevalue in thwe specified order
-
-delete notasMensais[1] // will delete the "8.9" but the space of order 1 keeps empty. Look:
-// It displays "<1 empty item>" in the "1" place.
-console.log(notasMensais)
+        delete notasMensais[1] // will delete the "8.9" but the space of order 1 keeps empty. Look:
+        // It displays "<1 empty item>" in the "1" place.
+        console.log(notasMensais)
 
 //Let's check what happens in I insert something in "1":
 
