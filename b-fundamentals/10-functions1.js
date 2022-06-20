a112b239be10d09ce9b430ade25dc41a59f15abd
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-20 17:47:38 -0300

    Create 10-functions1.js

---
+++ b-fundamentals/10-functions1.js
@@ -0,0 +1,12 @@
+
+console.log(typeof Object) // Objects are FUNCTIONS!!!
+
+// class is a nwe structure created in ECMA Script 2015:
+
+class product {}
+console.log(typeof product) // Classes are FUNCTIONS!!!
+
+console.log("JAVASCRIPT IS HIGHLY CENTERED IN FUNCTIONS.")
+
+// NO RETURN FUNCTION IN JS
+
