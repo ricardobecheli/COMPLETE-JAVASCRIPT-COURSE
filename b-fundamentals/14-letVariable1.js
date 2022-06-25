85daba4f44821b1ce45e56a79f1e010f163f9dce
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-25 10:51:29 -0300

    Scope - beginning.

--- b-fundamentals/14-letVariable1.js
+++ b-fundamentals/14-letVariable1.js
@@ -1,4 +1,5 @@
 var sample = 1
+
 {
     let sample = 2
     console.log("inside = ", sample)
@@ -8,10 +9,13 @@ console.log("out = ", sample)
 // let is more restricted = keeps the local scope.
 // If there's not "sample" inside, JS will return and display only the global:
 
-var sample = 1
 {
     let sampless = 2
-    console.log("inside, look at that now! = ", sample)
-    // Will look for "sample" evrywhere and will find it globally.
+    console.log("What's inside,now? No variable named sampless. So, JS will look for a variable named sample in a larger scope, globally, untill JS finds way up in the code a variable named sample. = ", sample)
+    // 
 }
-console.log("out = ", sample)
+console.log("Here outside the function a variable named sample is easily found: ", sample)
+console.log("In short:")
+console.log("Pay attention:")
+console.log("var has 2 scopes: function and global")
+console.log("let has 3 scopes: BLOCK, function and global. Is that all? Let's study more and check...")
