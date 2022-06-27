2eef0c4c1fc0e1b997427f39a27f693b4eefaff2
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-06-27 17:25:21 -0300

    Create 20-notationPoint.js

---
+++ b-fundamentals/20-notationPoint.js
@@ -0,0 +1,10 @@
+/* We will:
+1) Create a funcion;
+2) Define / create dynamically an internal attibute of this function;
+3) Access it using the poit.
+4) BRACKETS CAN DO IT TOO.
+*/
+
+const obj1 = {}
+obj1.name = 'Ball'
+obj1['name'] = 'baqlkl'
