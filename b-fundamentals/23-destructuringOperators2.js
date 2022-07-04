f639394f63bb78ebee7445cb26d87edfb6eeb07d
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-07-04 12:26:51 -0300

    Update 23-destructuringOperators2.js

--- b-fundamentals/23-destructuringOperators2.js
+++ b-fundamentals/23-destructuringOperators2.js
@@ -1,4 +1,21 @@
 //DESTRUCTURING IN ARRAYS
 
+/*
+            Remembering how an array is MADE:
+            const notasMensais = [7.7, 8.9, 6.3, 9.2]
+            four positions in this array: from zero to 3.
+*/
+
 const [a] = [10]
+/*  - in "[a]" here I'm using the brackets to DESTRUCTURE not to build.
+    - Just created a a variable "a" that received "10"... wait a second!!!
+    
+    THIS TEACHER'S LESSON IS VERY CONFUSED 
+    BECAUSE HE MIXED SEVERAL CONCEPTS BESIDES DESTRUCTURING, AT THE SAME TIME.
+    A TOTAL MESS!
+    SO WATCH THESE VIDEOS FIRST:
+    https://www.youtube.com/watch?v=NIq3qLaHCIs
+    https://www.youtube.com/watch?v=rpYg2M2lNAE
+    https://www.youtube.com/watch?v=fsybVOVcNg0
+*/
 console.log(a)
