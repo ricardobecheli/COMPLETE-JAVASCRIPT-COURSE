0585b1a080f936c211ee3f31b889000a92ab5f54
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-07-17 10:17:08 -0300

    Update 25-Arithmetic Operators.js

--- b-fundamentals/25-Arithmetic Operators.js
+++ b-fundamentals/25-Arithmetic Operators.js
@@ -4,5 +4,11 @@ let [a, b, c, d] = [13, 5, 1, 15]
 const soma = (a + b + c + d)
 console.log(soma)
 
-const divisao = (a % b) + (c / d)
+const divisao = (a % b) + (c / d) // const!
 console.log(divisao)
+
+let contas = (a*2) + b**2 + (100/c + 100) / d //let!
+console.log(contas)
+
+contas = 7
+console.log(contas)
