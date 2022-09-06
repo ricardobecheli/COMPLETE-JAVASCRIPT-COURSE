20094ab1960e50f7c85173f57fb9abdc0d471d46
ricardobecheli <ricardobarrosbecheli@gmail.com>
2022-09-06 17:25:26 -0300

    Update 22-destructuringOperators1.js

--- b-fundamentals/22-destructuringOperators1.js
+++ b-fundamentals/22-destructuringOperators1.js
@@ -1,28 +1,28 @@
- /*  DESTRUCTURING AN OBJECT {}
+/*  DESTRUCTURING AN OBJECT {}
  
-    I want to have access to 1 or more of these "INTERNAL DATA" from the object below, like nome, age, etc, and make them become VARIABLES.
+   I want to have access to 1 or more of these "INTERNAL DATA" from the object below, like nome, age, etc, and make them become VARIABLES.
 
-    They're inside an object in this lesson or inside an array (next lesson).
+   They're inside an object in this lesson or inside an array (next lesson).
 
-    You could say:
+   You could say:
 
-    - Easy. Just use the dot notation, as follows:
+   - Easy. Just use the dot notation, as follows:
 
-            let nome = person.nome 
-            const age = person.age
+           let nome = person.nome 
+           const age = person.age
 
-    Ok, that is the standard way:
+   Ok, that is the standard way:
 
-            First comes the type (let, const or var)
-            After the type, the identifier (the name of the variable),
-            then the equal sign (=, that means "receives")
-            and at last the data itself (the value).
+           First comes the type (let, const or var)
+           After the type, the identifier (the name of the variable),
+           then the equal sign (=, that means "receives")
+           and at last the data itself (the value).
 
-    But now we're gonna use the destructuring operator, a simplified way to do the same.
+   But now we're gonna use the destructuring operator, a simplified way to do the same.
 
-    Look below this line: "person" here is an OBJECT.
-    The Whole structure and data form this object.
- */
+   Look below this line: "person" here is an OBJECT.
+   The Whole structure and data form this object.
+*/
 const person = {
     nome: "Ana",
     age: 21,
@@ -45,16 +45,16 @@ console.log(nome + " | and now, in this row, one at a time. Here only " + nome) 
 console.log(age + " | and now, in this row, one at a time. Here only " + age) // 21
 
 
-const {nome: n, age: a} = person // I can change the const or let or var names.
+const { nome: n, age: a } = person // I can change the const or let or var names.
 console.log(n, a) // Ana, 21  | By changing the name, the "storage" place becomes more independent, detached, whatever, maybe avoiding confusion.
 
-const { sobrenome, bemHumorada = true, outraConst = null} = person
+const { sobrenome, bemHumorada = true, outraConst = null } = person
 // If I try to take an inexistent attribute from the object: Undefined or true. If you set it as true, it gets a "standard value" (- If there's nothing there, please assume true as a standard.).
 
 console.log(sobrenome, bemHumorada, outraConst) // undefined true null
 // Any other thing but undefined makes clear that someone has given prevoius attention to that constant or variable.
 
-const {courses: {engineering, chemistryGrade, cooking, tvSetWhatever}} = person
+const { courses: { engineering, chemistryGrade, cooking, tvSetWhatever } } = person
 
 console.log(engineering, chemistryGrade, cooking, tvSetWhatever)
 // undergraduate 10 null undefined
@@ -71,5 +71,8 @@ If you try to extract
     const { f1, soccer } = games
 Will resul in error, because there´s no "games" here.
 */
-let stockCar = 10
+let stockCar = barrichello = strudel
+let barrichello = 111
+let strudel = 25 * 4
+
 console.log(stockCar)
